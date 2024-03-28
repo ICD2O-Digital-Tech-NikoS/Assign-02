b9082137c793e18068e9ce60f33508bc4380271b
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-03-28 01:51:44 +0000

    i

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: march 2022
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// function to calculate percentage
+function calculatePercentage() {
+  // declaring variable for 
+  let testMarks = document.getElementById("testmarks").value;
+
+  // declaring variable for wage with value set as value inputed by user 
+  let testTotal = document.getElementById("testtotal").value;
+
+  // calculates result by multiplying the inputed wage and hours together to get total pay
+  let result = testMarks/testTotal * 100;
+  document.getElementById("user-info").innerHTML = "Your test score was " + result.toFixed(0) + "%.";
+
+}
+
+
+// Take the score and divide it by the number of questions and multiply it by 100 to get the percentage
